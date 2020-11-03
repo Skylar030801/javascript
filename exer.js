c441bb9473a8457f86879ca4c7f991b44c7073e7
skylarH <elantrahufkie@gmail.com>
2020-11-03 16:40:25 +0200

    exercise

---
+++ exer.js
@@ -0,0 +1,12 @@
+let name = "Skylar";
+surname = "Hufkie";
+document.write(name + "<br/>");
+document.write(surname);
+
+let numberVar = "5";
+document.write(numberVar);
+
+const me = { name1: "skylar", surname1: "Hufkie" };
+let { name1, surname1 } = me;
+document.write(name1 + "<br/>");
+document.write(surname1 + "<br/>");
